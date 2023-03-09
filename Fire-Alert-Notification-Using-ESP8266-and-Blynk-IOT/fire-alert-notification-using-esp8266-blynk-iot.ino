e73d0db76d619c9df8d4ec1396156707315f0ead
shameermohamed <shameer50@gmail.com>
2023-03-09 18:35:06 +0530

    Create fire-alert-notification-using-esp8266-blynk-iot.ino

---
+++ Fire-Alert-Notification-Using-ESP8266-and-Blynk-IOT/fire-alert-notification-using-esp8266-blynk-iot.ino
@@ -0,0 +1,50 @@
+#define BLYNK_TEMPLATE_ID "TMPL9Efi0t2B"
+#define BLYNK_TEMPLATE_NAME "Fire Alert Notification"
+#define BLYNK_AUTH_TOKEN "zVvOkRdha4GrmMmQcxeSwfjklfgdjhyVgsJTl7jF"
+
+
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+ 
+char auth[] = BLYNK_AUTH_TOKEN;
+
+char ssid[] = "WiFi Username";  // type your wifi name
+char pass[] = "WiFi Password";  // type your wifi password
+ 
+
+BlynkTimer timer;
+int flag=0; 
+void sendSensor(){ 
+   int isButtonPressed = digitalRead(D1);
+  if (isButtonPressed==0 && flag==0) {
+    Serial.println("Fire in the House");  
+    Blynk.email("testemail@gmail.com", "Alert", "Fire Detected!");
+    Blynk.logEvent("fire_alert","Fire Detected");
+    
+    flag=1;
+  }
+  else if (isButtonPressed==1)
+  {
+    flag=0;
+    
+  }
+
+
+ 
+}
+
+void setup(){
+  pinMode(D1, INPUT);
+ 
+   Serial.begin(115200);
+  Blynk.begin(auth, ssid, pass);
+  //dht.begin();
+  timer.setInterval(5000L, sendSensor);
+}
+
+void loop(){
+  Blynk.run();
+  timer.run();
+}
